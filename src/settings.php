dfd7609169a05719a19422d755c3adb114531113
Mikołaj Panecki <mikolaj@panecki.dev>
2026-06-16 11:43:08 +0200

    Preparing docker env and some package updates

--- src/settings.php
+++ src/settings.php
@@ -6,17 +6,17 @@ return [
 
         // Twig renderer
         'twig' => [
-            'template_path' => __DIR__ . '\..\resources\views',
+            'template_path' => dirname(__DIR__) . '/resources/views',
             'cache' => false,
         ],
 
         // Database credentials
         'db' => [
             'driver' => 'mysql',
-            'host' => 'localhost', // Database host
+            'host' => 'db', // Database host
             'database' => 'economy_redux', // Database name
-            'username' => 'root', // Database user
-            'password' => '', // Database password
+            'username' => 'economy', // Database user
+            'password' => 'economy', // Database password
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
             'prefix' => '',
